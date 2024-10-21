e694d3fb134a0fbc8782c05ad80eb57ae625f179
haotian.chen <haotian.chen@cmexpro.com>
2024-10-21 16:18:10 +0800

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<h1>Different way to Sign-in with solana</h1>
+<nav>
+    <ul>
+        <li><button on:click={() => goto('/dev')}>Go to Connection Solana Wallet by Direct</button></li>
+        <li><button on:click={() => goto('/org')}>Go to Connection Solana Wallet by wallet-adapter-ui</button></li>
+    </ul>
+</nav>
+
+<script>
+    import { goto } from '$app/navigation';
+</script>
+
+
+
+<slot></slot>
